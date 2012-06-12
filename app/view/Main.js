dfd96fa3262b1d13d58efe4df86cb99152141e95
speedkuk <speedkuk@gmail.com>
2012-06-12 16:46:00 +0900

    Edit Mani.js

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,51 +1,115 @@
 Ext.define("PMStouch.view.Main", {
     extend: 'Ext.tab.Panel',
-    requires: ['Ext.TitleBar', 'Ext.dataview.List'],
+    requires: ['Ext.TitleBar', 'Ext.dataview.List', 'Ext.form.Panel', 'Ext.field.Select', 'Ext.form.FieldSet', 'Ext.navigation.View'],
     
     config: {
-        tabBarPosition: 'bottom',
+		
+		items: [
+		            {
+		                xtype: 'navigationview',   
+		                title: 'Tab 1',
+		                iconCls: 'info',
+		                items: [
+		                    {
+		                        xtype: 'formpanel',
+		                        items: [
+		                            {
+		                                xtype: 'selectfield',
+		                                label: 'User',
+										options: [
+										                        {text: 'First Option',  value: 'first'},
+										                        {text: 'Second Option', value: 'second'},
+										                        {text: 'Third Option',  value: 'third'}
+										                    ]
+										// defaultPhonePickerConfig : {
+										// 											slots: [{
+										// 												title: 'User',
+										// 												store: 'RasViewResourceListOut'
+										// 											}]	
+										// 										}
+		                            },
+		                            {
+		                                xtype: 'fieldset',
+		                                title: 'Recent Info.',
+		                                items: [
+		                                    {
+		                                        xtype: 'textfield',
+		                                        label: 'T. Time'
+		                                    },
+		                                    {
+		                                        xtype: 'textfield',
+		                                        label: 'Event'
+		                                    },
+		                                    {
+		                                        xtype: 'textfield',
+		                                        label: 'P. Code'
+		                                    }
+		                                ]
+		                            },
+		                            {
+		                                xtype: 'container',
+		                                layout: {
+		                                    type: 'hbox'
+		                                },
+		                                items: [
+		                                    {
+		                                        xtype: 'button',
+		                                        width: '',
+		                                        text: 'Start',
+		                                        flex: 1
+		                                    },
+		                                    {
+		                                        xtype: 'button',
+		                                        text: 'End',
+		                                        flex: 1
+		                                    },
+		                                    {
+		                                        xtype: 'button',
+		                                        text: 'More',
+		                                        flex: 1
+		                                    }
+		                                ]
+		                            }
+		                        ]
+		                    }
+		                ]
+		            },
+		            {
+		                xtype: 'container',
+		                title: 'Tab 2',
+		                iconCls: 'info'
+		            },
+		            {
+		                xtype: 'container',
+		                title: 'Tab 3',
+		                iconCls: 'info'
+		            }
+		        ],
+		        tabBar: {
+		            docked: 'bottom'
+		        }
+		  
         
-        items: [
-            {
-                title: 'Welcome',
-                iconCls: 'home',
-                
-                styleHtmlContent: true,
-                scrollable: true,
-
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
-                },
-                
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
-            },
-            {
-                title: 'Resource',
-				iconCls: 'home',
-                xtype : 'reslist'
-            },
-			{
-				title: 'Resource History',
-				iconCls: 'home',
-                xtype : 'reshistory'
-			},
-			{
-				title: 'Event',
-				iconCls: 'home',
-                xtype : 'eventlist'
-			},
-			{
-				title: 'Project Code',
-				iconCls: 'home',
-                xtype : 'projectcode'
-			}
+            // {
+            //                 title: 'Resource',
+            // 				iconCls: 'home',
+            //                 xtype : 'reslist'
+            //             },
+            // 			{
+            // 				title: 'Resource History',
+            // 				iconCls: 'home',
+            //                 xtype : 'reshistory'
+            // 			},
+            // 			{
+            // 				title: 'Event',
+            // 				iconCls: 'home',
+            //                 xtype : 'eventlist'
+            // 			},
+            // 			{
+            // 				title: 'Project Code',
+            // 				iconCls: 'home',
+            //                 xtype : 'projectcode'
+            // 			}
 			
-        ]
-    }
+     }
 });
