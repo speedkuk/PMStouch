b86a75b4e6430cc1a1e72adba9dd70c3e165a4ec
speedkuk <speedkuk@gmail.com>
2012-06-18 17:27:29 +0900

    Add resDesc

--- app/view/ResourceList.js
+++ app/view/ResourceList.js
@@ -2,7 +2,7 @@ Ext.define('PMStouch.view.ResourceList', {
 	extend : 'Ext.List',
 	xtype : 'reslist',
 	config : {
-	    itemTpl: '{resId}',
+	    itemTpl: '{resId} - {resDesc}',
 		title : '직원 아이디 선택',
 	    store : 'RasViewResourceListOut'
 	}
